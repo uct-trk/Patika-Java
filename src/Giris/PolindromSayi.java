c6aa9c5c5f3a8f25f16286f3d4528ea3952541f7
Ugurcan Turk <ugurcan.uut82@gmail.com>
2022-01-26 23:41:17 +0300

    recursive

---
+++ src/Giris/PolindromSayi.java
@@ -0,0 +1,7 @@
+package Giris;
+
+public class PolindromSayi {
+    public static void main(String[] args) {
+
+    }
+}
--- src/Giris/RecursiveFunc.java
+++ src/Giris/RecursiveFunc.java
@@ -2,15 +2,46 @@ package Giris;
 
 public class RecursiveFunc {
 
-    static  int f(int n){
+    static int basamaklarToplami(int sayi){
+        System.out.println("sayı => " + sayi);
+        if (sayi == 0){
+            return 0;
+        }else {
+            System.out.println("Sayı: " + sayi);
+            return sayi % 10 + basamaklarToplami(sayi / 10);
+        }
+    }
 
+    static  int f(int n){
+        System.out.println("n => " + n);
         if (n == 1){
             return 1;
         }
-        return f(n - 1) + n;
+        int result = f(n - 1) + n;
+        System.out.println("result => " + result);
+        return result;
+    }
+
+    static int as(int i) {
+        if (i < 2) return 1;
+        else return (i * as(i - 1));
+    }
+
+    static void recursiveMethod(int num) {
+        num--;
+        if (num == 0)
+            return;
+        System.out.print(num + ",");
+        recursiveMethod(num);
     }
 
     public static void main(String[] args) {
-        System.out.println(f(4));
+        System.out.println(f(3));
+
+        System.out.println("Basamak toplami: "+basamaklarToplami(45612));
+
+        System.out.println(as(3));
+
+        recursiveMethod(4);
     }
 }
