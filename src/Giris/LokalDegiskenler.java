03941072e2644ff84d70cf6740d033682a7b3a2e
Ugurcan Turk <ugurcan.uut82@gmail.com>
2022-01-26 20:13:06 +0300

    lokal degiskenler

---
+++ src/Giris/LokalDegiskenler.java
@@ -0,0 +1,32 @@
+package Giris;
+
+public class LokalDegiskenler {
+
+    static void number(int x, int z){
+        int y = 2;
+        x = x * y;
+        System.out.print(x);
+    }
+
+
+    public static void main(String[] args) {
+
+        int y = 3;
+        int z = 2;
+        if (y != z){
+            int x = 20;
+        }
+        int x = 50;
+        number(x,z);
+
+        String a = "Patika.dev";
+        kodluyoruz(a);
+        System.out.println(a);
+
+    }
+    static void kodluyoruz(String y)
+    {
+        String a = "Java101";
+        y = "Kodluyoruz";
+    }
+}
