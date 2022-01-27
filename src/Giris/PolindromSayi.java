4407cab79c2351bc24dd30ab182b618a78bf9737
Ugurcan Turk <ugurcan.uut82@gmail.com>
2022-01-27 20:10:14 +0300

    polindrom sayı

--- src/Giris/PolindromSayi.java
+++ src/Giris/PolindromSayi.java
@@ -1,7 +1,27 @@
 package Giris;
 
 public class PolindromSayi {
-    public static void main(String[] args) {
 
+    static boolean isPolindrom(int number){
+        int temp = number, reverseNuber = 0, lastNumber;
+        while (temp != 0){
+            System.out.println("============");
+            System.out.println("Sayı => " + temp);
+            lastNumber = temp % 10;
+            System.out.println("Son Basamak => " + lastNumber);
+            reverseNuber = (reverseNuber * 10) + lastNumber;
+            System.out.println("Yeni Sayı => " + reverseNuber);
+            temp = temp / 10;
+            System.out.println("=============");
+        }
+        if (number == reverseNuber){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPolindrom(101));
     }
 }
