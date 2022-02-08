a79f8e308506684809eef5e6a13eeedf5ae6328e
Ugurcan Turk <ugurcan.uut82@gmail.com>
2022-02-08 13:49:59 +0300

    java math sinifi

---
+++ src/Giris/MathSinifi.java
@@ -0,0 +1,15 @@
+package Giris;
+
+public class MathSinifi {
+    public static void main(String[] args) {
+
+        System.out.println(Math.abs(-100));
+        System.out.println(Math.cbrt(8));
+        System.out.println(Math.ceil(2.2));
+        System.out.println(Math.floor(2.2));
+        System.out.println(Math.pow(8,2));
+        System.out.println(Math.random() * 100);
+        System.out.println(Math.round(8.5));
+        System.out.println(Math.sqrt(8));
+    }
+}
